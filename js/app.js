a357e8c904bf083626625e6bb317ea22362d350d
Nicolas <nicolas.sanpedro1@gmail.com>
2015-12-17 12:34:38 +0100

    First commit

--- js/app.js
+++ js/app.js
@@ -6,5 +6,17 @@ $(document).ready(function() {
     	var val = $(this).attr('href');
        	$('#par1').load(val);
     });
-    
-  }); 
+});
+
+
+$(function () {
+		$.getJSON('http://vps227573.ovh.net/u-11.json', function(data) {
+			var t = $('[elt]');
+			for (var i = 0; i < t.length; i ++) {
+
+				var attribut = $(t[i]).attr('elt');
+				var valeur = data [attribut];
+				$(t[i]).html(valeur);
+			}
+	});
+});
